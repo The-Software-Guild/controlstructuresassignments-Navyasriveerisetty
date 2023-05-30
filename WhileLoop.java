c3e680964247355250759afc0688f4456f61366b
Navyasriveerisetty <133081344+Navyasriveerisetty@users.noreply.github.com>
2023-05-30 14:29:01 +1000

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,17 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int num = 121;
+        int reversedNum = 0;
+        int originalNum = num;
+        while (num != 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+        if (originalNum == reversedNum) {
+            System.out.println(originalNum + " is a palindrome.");
+        } else {
+            System.out.println(originalNum + " is not a palindrome.");
+        }
+    }
+}
